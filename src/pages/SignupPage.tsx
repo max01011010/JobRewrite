b3ec2b513e1a0344a95fe646d753409c033e731e
[dyad] <max01011010@gmail.com>
2025-08-15 13:59:31 -0400

    [dyad] Reordered import and client directive in SignupPage.tsx and requested a full rebuild. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,5 +1,5 @@
-"use client";
 import React from 'react';
+"use client";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
